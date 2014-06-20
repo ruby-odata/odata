06cb534b27b0af9c4ef4a4d1a1794776a0be1658
jamesthompson <jamest@thereadyproject.com>
2014-06-20 02:33:48 -0600

    Added some basic documentation to OData::ServiceRegistry. [ci skip]

--- lib/odata/service_registry.rb
+++ lib/odata/service_registry.rb
@@ -1,9 +1,13 @@
 require 'singleton'
 
 module OData
+  # Provides a registry for keeping track of multiple OData::Service instances
   class ServiceRegistry
     include Singleton
 
+    # Add a service to the Registry
+    #
+    # @param service [OData::Service] service to add to the registry
     def add(service)
       initialize_instance_variables
       @services << service if service.is_a?(OData::Service) && !@services.include?(service)
@@ -11,16 +15,22 @@ module OData
       @services_by_url[service.service_url] = @services.find_index(service)
     end
 
+    # Lookup a service by URL or namespace
+    #
+    # @param lookup_key [String] the URL or namespace to lookup
+    # @return [OData::Service, nil] the OData::Service or nil
     def [](lookup_key)
       initialize_instance_variables
       index = @services_by_namespace[lookup_key] || @services_by_url[lookup_key]
       index.nil? ? nil : @services[index]
     end
 
+    # (see #add)
     def self.add(service)
       OData::ServiceRegistry.instance.add(service)
     end
 
+    # (see #[])
     def self.[](lookup_key)
       OData::ServiceRegistry.instance[lookup_key]
     end
