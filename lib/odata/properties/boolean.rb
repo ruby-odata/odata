6fa8e36728bdaeee6984766c80b2d648cfc8184f
James Thompson <james@buypd.com>
2014-06-25 15:13:41 -0600

    Finished up implementation of Boolean property type.

--- lib/odata/properties/boolean.rb
+++ lib/odata/properties/boolean.rb
@@ -9,8 +9,21 @@ module OData
         end
       end
 
+      def value=(new_value)
+        validate(new_value)
+        @value = new_value.to_s
+      end
+
       def type
         'Edm.Boolean'
+      end
+
+      private
+
+      def validate(value)
+        unless [0,1,'0','1','true','false',true,false].include?(value)
+          raise ArgumentError, 'Value is outside accepted range: true or false'
+        end
       end
     end
   end
--- spec/odata/properties/boolean_spec.rb
+++ spec/odata/properties/boolean_spec.rb
@@ -15,4 +15,48 @@ describe OData::Properties::Boolean do
   it { expect(falsey2.value).to eq(false) }
 
   it { expect(nily.value).to eq(nil) }
+
+  it { expect {truthy1.value = 'bad'}.to raise_error(ArgumentError) }
+
+  describe 'setting to false' do
+    let(:subject) { OData::Properties::Boolean.new('Truthy', 'true') }
+
+    it { expect(subject.value).to eq(true) }
+
+    it { expect(lambda {
+      subject.value = false
+      subject.value
+    }.call).to eq(false) }
+
+    it { expect(lambda {
+      subject.value = 'false'
+      subject.value
+    }.call).to eq(false) }
+
+    it { expect(lambda {
+      subject.value = '0'
+      subject.value
+    }.call).to eq(false) }
+  end
+
+  describe 'setting to true' do
+    let(:subject) { OData::Properties::Boolean.new('Falsey', 'false') }
+
+    it { expect(subject.value).to eq(false) }
+
+    it { expect(lambda {
+      subject.value = true
+      subject.value
+    }.call).to eq(true) }
+
+    it { expect(lambda {
+      subject.value = 'true'
+      subject.value
+    }.call).to eq(true) }
+
+    it { expect(lambda {
+      subject.value = '1'
+      subject.value
+    }.call).to eq(true) }
+  end
 end
