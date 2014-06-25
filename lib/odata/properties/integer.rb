252f93346872de6c32d92f6f5487386e948f0981
James Thompson <james@buypd.com>
2014-06-25 10:58:40 -0600

    Added max and min value checking on integer types and implemented Byte and SByte types.

--- lib/odata/properties/integer.rb
+++ lib/odata/properties/integer.rb
@@ -10,11 +10,28 @@ module OData
       end
 
       def value=(new_value)
+        validate(new_value.to_i)
         @value = new_value.to_i.to_s
       end
 
       def type
         'Edm.Int64'
+      end
+
+      private
+
+      def validate(value)
+        if value > max_value || value < min_value
+          raise ::ArgumentError, "Value is outside accepted range: #{min_value} to #{max_value}"
+        end
+      end
+
+      def min_value
+        @min ||= -(2**63)
+      end
+
+      def max_value
+        @max ||= (2**63)-1
       end
     end
 
@@ -22,18 +39,66 @@ module OData
       def type
         'Edm.Int16'
       end
+
+      private
+
+      def min_value
+        @min ||= -(2**31)
+      end
+
+      def max_value
+        @max ||= (2**31)-1
+      end
     end
 
     class Int32 < Integer
       def type
         'Edm.Int32'
       end
+
+      private
+
+      def min_value
+        @min ||= -(2**15)
+      end
+
+      def max_value
+        @max ||= (2**15)-1
+      end
     end
 
-    class Int64 < Integer
-      # def type
-      #   'Edm.Int64'
-      # end
+    class Int64 < Integer; end
+
+    class Byte < Integer
+      def type
+        'Edm.Byte'
+      end
+
+      private
+
+      def min_value
+        0
+      end
+
+      def max_value
+        @max ||= (2**8)-1
+      end
+    end
+
+    class SByte < Integer
+      def type
+        'Edm.SByte'
+      end
+
+      private
+
+      def min_value
+        @min ||= -(2**7)
+      end
+
+      def max_value
+        @max ||= (2**7)-1
+      end
     end
   end
 end
--- spec/odata/properties/integer_spec.rb
+++ spec/odata/properties/integer_spec.rb
@@ -5,6 +5,8 @@ describe OData::Properties::Integer do
   let(:subject16) { OData::Properties::Int16.new('Int16', '32') }
   let(:subject32) { OData::Properties::Int32.new('Int32', '32') }
   let(:subject64) { OData::Properties::Int64.new('Int64', '32') }
+  let(:subjectbyte) { OData::Properties::Byte.new('Byte', '32') }
+  let(:subjectsbyte) { OData::Properties::SByte.new('SByte', '32') }
 
   it { expect(subject.type).to eq('Edm.Int64') }
   it { expect(subject.value).to eq(32) }
@@ -18,17 +20,27 @@ describe OData::Properties::Integer do
   it { expect(subject64.type).to eq('Edm.Int64') }
   it { expect(subject64.value).to eq(32) }
 
+  it { expect(subjectbyte.type).to eq('Edm.Byte') }
+  it { expect(subjectbyte.value).to eq(32) }
+
+  it { expect(subjectsbyte.type).to eq('Edm.SByte') }
+  it { expect(subjectsbyte.value).to eq(32) }
+
   describe '#value=' do
     before(:example) do
       subject.value = 128
       subject16.value = 128
       subject32.value = 128
       subject64.value = 128
+      subjectbyte.value = 12
+      subjectsbyte.value = 12
     end
 
     it { expect(subject.value).to eq(128) }
     it { expect(subject16.value).to eq(128) }
     it { expect(subject32.value).to eq(128) }
     it { expect(subject64.value).to eq(128) }
+    it { expect(subjectbyte.value).to eq(12) }
+    it { expect(subjectsbyte.value).to eq(12) }
   end
 end
