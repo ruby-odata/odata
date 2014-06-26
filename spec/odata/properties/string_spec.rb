5d8f5dc155c98af014f8de8c060da61b3dc67696
James Thompson <james@buypd.com>
2014-06-26 10:36:34 -0600

    Added more specs around String allowing nil values.

--- spec/odata/properties/string_spec.rb
+++ spec/odata/properties/string_spec.rb
@@ -11,6 +11,11 @@ describe OData::Properties::String do
     subject.value
   }.call).to eq('Another example') }
 
+  it { expect(lambda {
+    subject.value = nil
+    subject.value
+  }.call).to eq(nil) }
+
   describe '#is_unicode?' do
     let(:not_unicode) { OData::Properties::String.new('Stringy', 'This is an example', unicode: false) }
 
@@ -19,5 +24,12 @@ describe OData::Properties::String do
 
     it { expect(subject.value.encoding).to eq(Encoding::UTF_8) }
     it { expect(not_unicode.value.encoding).to eq(Encoding::ASCII) }
+  end
+
+  describe 'when #allows_nil? is false' do
+    let(:subject) { OData::Properties::String.new('Stringy', 'This is an example', allows_nil: false) }
+
+    it { expect {subject.value = nil}.to raise_error(ArgumentError) }
+    it { expect {subject.value = 'Test'}.not_to raise_error }
   end
 end
