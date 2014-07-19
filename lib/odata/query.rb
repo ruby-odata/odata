f704d3f7cd749e2a0f2838c5f7011ee43a405d01
James Thompson <james@buypd.com>
2014-07-18 22:41:20 -0600

    Added YARD documentation to OData::Query.

--- lib/odata/query.rb
+++ lib/odata/query.rb
@@ -1,37 +1,55 @@
 module OData
   class Query
+    # Create a new Query for the provided EntitySet
+    # @param entity_set [OData::EntitySet]
     def initialize(entity_set)
       @entity_set = entity_set
       setup_empty_criteria_set
     end
 
+    # Adds a filter criteria to the query.
+    # @param criteria
     def where(criteria)
 
     end
 
+    # Adds a filter criteria to the query with 'and' logical operator.
+    # @param criteria
     def and(criteria)
 
     end
 
+    # Adds a filter criteria to the query with 'or' logical operator.
+    # @param criteria
     def or(criteria)
 
     end
 
+    # Add skip criteria to query.
+    # @param value [to_i]
+    # @return [self]
     def skip(value)
       criteria_set[:skip] = value.to_i
       self
     end
 
+    # Add limit criteria to query.
+    # @param value [to_i]
+    # @return [self]
     def limit(value)
       criteria_set[:top] = value.to_i
       self
     end
 
+    # Add inline count criteria to query.
+    # @return [self]
     def include_count
       criteria_set[:inline_count] = true
       self
     end
 
+    # Convert Query to string.
+    # @return [String]
     def to_s
       [entity_set.name, assemble_criteria].compact.join('?')
     end
