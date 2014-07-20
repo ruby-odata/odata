0391b714411c54fb58314c949168d6b6e496636e
James Thompson <james@buypd.com>
2014-07-20 11:10:28 -0600

    Fleshed out implementation of OData::Query::Criteria.

--- lib/odata/query/criteria.rb
+++ lib/odata/query/criteria.rb
@@ -17,6 +17,65 @@ module OData
         @operator = options[:operator]
         @value    = options[:value]
       end
+
+      # Sets up equality operator.
+      # @param value [to_s]
+      # @return [self]
+      def eq(value)
+        set_operator_and_value(:eq, value)
+      end
+
+      # Sets up non-equality operator.
+      # @param value [to_s]
+      # @return [self]
+      def ne(value)
+        set_operator_and_value(:ne, value)
+      end
+
+      # Sets up greater-than operator.
+      # @param value [to_s]
+      # @return [self]
+      def gt(value)
+        set_operator_and_value(:gt, value)
+      end
+
+      # Sets up greater-than-or-equal operator.
+      # @param value [to_s]
+      # @return [self]
+      def ge(value)
+        set_operator_and_value(:ge, value)
+      end
+
+      # Sets up less-than operator.
+      # @param value [to_s]
+      # @return [self]
+      def lt(value)
+        set_operator_and_value(:lt, value)
+      end
+
+      # Sets up less-than-or-equal operator.
+      # @param value [to_s]
+      # @return [self]
+      def le(value)
+        set_operator_and_value(:le, value)
+      end
+
+      # Returns criteria as query-ready string.
+      def to_s
+        "#{property} #{operator} #{value_for_string}"
+      end
+
+      private
+
+      def set_operator_and_value(operator, value)
+        @operator = operator
+        @value = value
+        self
+      end
+
+      def value_for_string
+        value.is_a?(String) ? "'#{value}'" : value
+      end
     end
   end
 end
--- spec/odata/query/criteria_spec.rb
+++ spec/odata/query/criteria_spec.rb
@@ -9,4 +9,61 @@ describe OData::Query::Criteria do
   it { expect(subject).to respond_to(:operator) }
   it { expect(subject).to respond_to(:value) }
   it { expect(subject).to respond_to(:to_s) }
+
+  it { expect(subject).to respond_to(:eq) }
+  it { expect(subject).to respond_to(:ne) }
+  it { expect(subject).to respond_to(:gt) }
+  it { expect(subject).to respond_to(:ge) }
+  it { expect(subject).to respond_to(:lt) }
+  it { expect(subject).to respond_to(:le) }
+
+  describe 'operator method' do
+    it '#eq sets up criteria properly' do
+      criteria = subject.eq('Bread')
+      expect(criteria).to eq(subject)
+      expect(criteria.operator).to eq(:eq)
+      expect(criteria.value).to eq('Bread')
+      expect(criteria.to_s).to eq("Name eq 'Bread'")
+    end
+
+    it '#ne sets up criteria properly' do
+      criteria = subject.ne('Bread')
+      expect(criteria).to eq(subject)
+      expect(criteria.operator).to eq(:ne)
+      expect(criteria.value).to eq('Bread')
+      expect(criteria.to_s).to eq("Name ne 'Bread'")
+    end
+
+    it '#gt sets up criteria properly' do
+      criteria = subject.gt(5)
+      expect(criteria).to eq(subject)
+      expect(criteria.operator).to eq(:gt)
+      expect(criteria.value).to eq(5)
+      expect(criteria.to_s).to eq('Name gt 5')
+    end
+
+    it '#ge sets up criteria properly' do
+      criteria = subject.ge(5)
+      expect(criteria).to eq(subject)
+      expect(criteria.operator).to eq(:ge)
+      expect(criteria.value).to eq(5)
+      expect(criteria.to_s).to eq('Name ge 5')
+    end
+
+    it '#lt sets up criteria properly' do
+      criteria = subject.lt(5)
+      expect(criteria).to eq(subject)
+      expect(criteria.operator).to eq(:lt)
+      expect(criteria.value).to eq(5)
+      expect(criteria.to_s).to eq('Name lt 5')
+    end
+
+    it '#le sets up criteria properly' do
+      criteria = subject.le(5)
+      expect(criteria).to eq(subject)
+      expect(criteria.operator).to eq(:le)
+      expect(criteria.value).to eq(5)
+      expect(criteria.to_s).to eq('Name le 5')
+    end
+  end
 end
