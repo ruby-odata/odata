b244875102cce8aca9f0061b4d576d992f21903e
James Thompson <james@buypd.com>
2014-10-10 22:16:18 -0600

    Added additional test coverage for OData::ComplexType.

--- spec/odata/complex_type_spec.rb
+++ spec/odata/complex_type_spec.rb
@@ -13,10 +13,12 @@ describe OData::ComplexType, vcr: {cassette_name: 'complex_type_specs'} do
   it { expect(subject).to respond_to(:property_names) }
   it { expect(subject).to respond_to(:[]) }
   it { expect(subject).to respond_to(:[]=) }
+  it { expect(subject).to respond_to(:type) }
 
   describe 'is properly parsed from service metadata' do
     it { expect(subject.name).to eq('Address') }
     it { expect(subject.namespace).to eq('ODataDemo') }
+    it { expect(subject.type).to eq('ODataDemo.Address') }
     it { expect(subject.property_names).to eq(%w{Street City State ZipCode Country}) }
 
     it { expect(subject['Street']).to be_nil }
@@ -30,5 +32,29 @@ describe OData::ComplexType, vcr: {cassette_name: 'complex_type_specs'} do
     it { subject['State'] = 'Test'; expect(subject['State']).to eq('Test') }
     it { subject['ZipCode'] = 'Test'; expect(subject['ZipCode']).to eq('Test') }
     it { subject['Country'] = 'Test'; expect(subject['Country']).to eq('Test') }
+  end
+
+  describe '#to_xml' do
+    let(:builder) do
+      Nokogiri::XML::Builder.new do |xml|
+        xml.entry('xmlns'           => 'http://www.w3.org/2005/Atom',
+                  'xmlns:data'      => 'http://schemas.microsoft.com/ado/2007/08/dataservices',
+                  'xmlns:metadata'  => 'http://schemas.microsoft.com/ado/2007/08/dataservices/metadata') do
+          subject.to_xml(xml)
+        end
+      end
+    end
+    let(:xml) { Nokogiri::XML(builder.to_xml) }
+
+    before(:each) do
+      xml.remove_namespaces!
+    end
+
+    it { expect(xml.xpath("/entry/Address[@type='ODataDemo.Address']").count).to eq(1) }
+    it { expect(xml.xpath('/entry/Address/Street').count).to eq(1) }
+    it { expect(xml.xpath('/entry/Address/City').count).to eq(1) }
+    it { expect(xml.xpath('/entry/Address/State').count).to eq(1) }
+    it { expect(xml.xpath('/entry/Address/ZipCode').count).to eq(1) }
+    it { expect(xml.xpath('/entry/Address/Country').count).to eq(1) }
   end
 end
