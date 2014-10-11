7e7174049271ec8f1d3d0131682326124d22b99b
James Thompson <james@buypd.com>
2014-10-10 22:33:26 -0600

    Added additional test coverage for OData::Proxy#[].

--- spec/odata/association/proxy_spec.rb
+++ spec/odata/association/proxy_spec.rb
@@ -15,5 +15,21 @@ describe OData::Association::Proxy, vcr: {cassette_name: 'association_proxy_spec
     it { expect(subject['ProductDetail']).to be_a(OData::Entity) }
     it { expect(subject['Categories']).to be_a(Enumerable) }
     it { expect(subject['Categories'].first).to be_a(OData::Entity) }
+
+    context 'when no links exist for an entity' do
+      before(:each) do
+        expect(entity).to receive(:links) do
+          { 'ProductDetail' => nil, 'Categories' => nil }
+        end
+      end
+
+      context 'for a many association' do
+        it { expect(subject['Categories']).to eq([]) }
+      end
+
+      context 'for a singular association' do
+        it { expect(subject['ProductDetail']).to eq(nil) }
+      end
+    end
   end
 end
