b91a933226443ee2928e42560457fc50b624e48f
Juan Wajnerman <jwajnerman@manas.com.ar>
2014-10-24 12:16:16 -0300

    Fix Query::Result not jumping to following pages while iterating

--- lib/odata/query/result.rb
+++ lib/odata/query/result.rb
@@ -19,7 +19,7 @@ module OData
       def each(&block)
         process_results(&block)
         until next_page.nil?
-          result = service.execute(next_page_url)
+          @result = service.execute(next_page_url)
           process_results(&block)
         end
       end
